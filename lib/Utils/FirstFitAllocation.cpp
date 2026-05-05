5b24dec6f7d90cca9e8783e8e4c0d72aa6cca978
bernborgess <bernborgess@outlook.com>
2026-05-05 14:25:20 -0300

    ref: Remove commented out debug code

--- lib/Utils/FirstFitAllocation.cpp
+++ lib/Utils/FirstFitAllocation.cpp
@@ -4,57 +4,62 @@
 #include "llvm/Support/raw_ostream.h"
 #include <list>
 
-size_t allocate(size_t bufferSize, std::list<std::pair<size_t, size_t>> &freeIntervals);
+size_t allocate(size_t bufferSize,
+                std::list<std::pair<size_t, size_t>> &freeIntervals);
 void merge_intervals(std::list<std::pair<size_t, size_t>> &freeIntervals);
-void deallocate(size_t startPos, size_t bufferSize, std::list<std::pair<size_t, size_t>> &freeIntervals);
+void deallocate(size_t startPos, size_t bufferSize,
+                std::list<std::pair<size_t, size_t>> &freeIntervals);
 
 namespace rarog {
 
+std::pair<llvm::SmallVector<size_t>, size_t> first_fit_allocation(
+    llvm::SmallVector<std::tuple<size_t, size_t, size_t>> buffers) {
+  llvm::SmallVector<std::tuple<size_t, size_t, size_t>> events;
+  llvm::DenseMap<size_t, size_t> bufferOffset;
 
-std::pair<llvm::SmallVector<size_t>, size_t> first_fit_allocation(llvm::SmallVector<std::tuple<size_t, size_t, size_t>> buffers) {
-    llvm::SmallVector<std::tuple<size_t, size_t, size_t>> events; 
-    llvm::DenseMap<size_t, size_t> bufferOffset;
+  size_t neededSize = 0;
 
-    size_t neededSize = 0;
+  std::list<std::pair<size_t, size_t>> freeIntervals;
 
-    std::list<std::pair<size_t, size_t>> freeIntervals;
+  for (auto [allocPos, freePos, size] : buffers) {
+    // Allocation event
+    events.emplace_back(allocPos, allocPos, size);
 
-    for (auto [allocPos, freePos, size] : buffers) {
-      // Allocation event
-      events.emplace_back(allocPos, allocPos, size);
+    // Deallocation event
+    events.emplace_back(freePos, allocPos, size);
 
-      // Deallocation event
-      events.emplace_back(freePos, allocPos, size);
+    neededSize += size;
+  }
+  llvm::sort(events.begin(), events.end());
 
-      neededSize += size;
-    } 
-    llvm::sort(events.begin(), events.end());
-    
-    // Create list of free intervals
-    freeIntervals = {{0, neededSize}};
-    neededSize = 0;
+  // Create list of free intervals
+  freeIntervals = {{0, neededSize}};
+  neededSize = 0;
 
-    llvm::SmallVector<size_t> allocationDecisions;
-    for (auto [pos, allocPos, size] : events) {
-      if (pos == allocPos) {
-        size_t offset = allocate(size, freeIntervals);
-        llvm::outs() << "\nAllocated buffer of size " << size << " at position " << offset << "\n";
-        bufferOffset[allocPos] = offset;
-        allocationDecisions.emplace_back(offset);
-        neededSize = std::max(neededSize, offset+size);
-      } else {
-        size_t offset = bufferOffset.at(allocPos);
-        deallocate(offset, size, freeIntervals);
-        llvm::outs() << "\nDeallocated buffer of size " << size << " at position " << offset << "\n";
-      }
+  llvm::SmallVector<size_t> allocationDecisions;
+  for (auto [pos, allocPos, size] : events) {
+    if (pos == allocPos) {
+      size_t offset = allocate(size, freeIntervals);
+      llvm::outs() << "\nAllocated buffer of size " << size << " at position "
+                   << offset << "\n";
+      bufferOffset[allocPos] = offset;
+      allocationDecisions.emplace_back(offset);
+      neededSize = std::max(neededSize, offset + size);
+    } else {
+      size_t offset = bufferOffset.at(allocPos);
+      deallocate(offset, size, freeIntervals);
+      llvm::outs() << "\nDeallocated buffer of size " << size << " at position "
+                   << offset << "\n";
     }
-
-    return {allocationDecisions, neededSize};
   }
 
+  return {allocationDecisions, neededSize};
 }
 
-size_t allocate(size_t bufferSize, std::list<std::pair<size_t, size_t>> &freeIntervals) {
+} // namespace rarog
+
+size_t allocate(size_t bufferSize,
+                std::list<std::pair<size_t, size_t>> &freeIntervals) {
   llvm::outs() << "\nTrying to allocate buffer of size " << bufferSize << "\n";
   llvm::outs() << "Available intervals:\n";
   for (auto [startPos, sz] : freeIntervals) {
@@ -63,8 +68,8 @@ size_t allocate(size_t bufferSize, std::list<std::pair<size_t, size_t>> &freeInt
   for (auto it = freeIntervals.begin(); it != freeIntervals.end(); ++it) {
     if (bufferSize <= it->second) {
       size_t startPos = it->first;
-      size_t endPos = startPos+bufferSize;
-      size_t newSize = it->second-bufferSize;
+      size_t endPos = startPos + bufferSize;
+      size_t newSize = it->second - bufferSize;
       if (newSize > 0) {
         freeIntervals.emplace(it, endPos, newSize);
       }
@@ -75,26 +80,26 @@ size_t allocate(size_t bufferSize, std::list<std::pair<size_t, size_t>> &freeInt
   return -1;
 }
 
-// We can change this to check only for the imediate previous and next element of the list
+// TODO: We can change this to check only for the imediate previous and next
+// element of the list
 void merge_intervals(std::list<std::pair<size_t, size_t>> &freeIntervals) {
-  llvm::SmallVector<std::list<std::pair<size_t,size_t>>::iterator> mergedIntervals, toErase;
+  llvm::SmallVector<std::list<std::pair<size_t, size_t>>::iterator>
+      mergedIntervals, toErase;
   size_t startPos, endPos;
-  // llvm::outs() << "\nMerging intervals\n";
   for (auto it = freeIntervals.begin(); it != freeIntervals.end(); ++it) {
     if (mergedIntervals.empty()) {
-      // llvm::outs() << "Offset: " << it->first << "; Size: " << it->second << "\n";
+
       mergedIntervals.emplace_back(it);
       startPos = it->first;
-      endPos = it->second+startPos;
+      endPos = it->second + startPos;
     } else {
-      // llvm::outs() << "Offset: " << it->first << "; Size: " << it->second << "\n";
-      // llvm::outs() << "StartPos: " << startPos << "; EndPos: " << endPos << "\n";
+
       if (it->first == endPos) {
         mergedIntervals.emplace_back(it);
         endPos = it->first + it->second;
       } else {
         if (mergedIntervals.size() > 1) {
-          freeIntervals.emplace(it, startPos, endPos-startPos);
+          freeIntervals.emplace(it, startPos, endPos - startPos);
           for (auto toEraseIt : mergedIntervals) {
             toErase.emplace_back(toEraseIt);
           }
@@ -102,13 +107,13 @@ void merge_intervals(std::list<std::pair<size_t, size_t>> &freeIntervals) {
         mergedIntervals.clear();
         mergedIntervals.emplace_back(it);
         startPos = it->first;
-        endPos = it->second+startPos;
+        endPos = it->second + startPos;
       }
     }
   }
 
   if (mergedIntervals.size() > 1) {
-    freeIntervals.emplace(mergedIntervals[0], startPos, endPos-startPos);
+    freeIntervals.emplace(mergedIntervals[0], startPos, endPos - startPos);
     for (auto toEraseIt : mergedIntervals) {
       toErase.emplace_back(toEraseIt);
     }
@@ -117,15 +122,10 @@ void merge_intervals(std::list<std::pair<size_t, size_t>> &freeIntervals) {
   for (auto it : toErase) {
     freeIntervals.erase(it);
   }
-
-  // llvm::outs() << "\nMerged intervals:\n";
-  // for (auto it = freeIntervals.begin(); it != freeIntervals.end(); ++it) {
-  //   llvm::outs() << "Offset: " << it->first << "; Size: " << it->second << "\n";
-  // }
 }
 
-void deallocate(size_t startPos, size_t bufferSize, std::list<std::pair<size_t, size_t>> &freeIntervals) {
-  // llvm::outs() << "Deallocating buffer starting at " << startPos << " with size " << bufferSize << "\n";
+void deallocate(size_t startPos, size_t bufferSize,
+                std::list<std::pair<size_t, size_t>> &freeIntervals) {
   for (auto it = freeIntervals.begin(); it != freeIntervals.end(); ++it) {
     if (it->first > startPos) {
       freeIntervals.emplace(it, startPos, bufferSize);
