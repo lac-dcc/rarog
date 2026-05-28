55ccaf7aed2394fb90300fde4cfc9282488abde4
bernborgess <bernborgess@outlook.com>
2026-05-28 11:02:16 -0300

    Test: Turning bennu files into linalg

---
+++ run_bennu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get bennu models (3GB)
+: '
+wget https://github.com/git-lfs/git-lfs/releases/download/v3.7.1/git-lfs-linux-amd64-v3.7.1.tar.gz
+tar -xzf git-lfs-linux-amd64-v3.7.1.tar.gz
+cd git-lfs-3.7.1/
+sudo ./install.sh 
+git lfs install
+cd ..
+git clone https://github.com/lac-dcc/bennu
+cd bennu
+git lfs track "*.onnx"
+git lfs pull
+'
+export MODEL_PATH="${MODEL_PATH:-$HOME/repos/bennu/models}"
+
+# ONNX to LINALG
+for model_name in $(ls -1 $MODEL_PATH/*.onnx); do
+    echo "* Create linalg $(basename ${model_name%.onnx})"
+    MODEL_NAME=$(basename ${model_name%.onnx}) ./scripts/create_linalg.sh
+done
+
+# # LINALG to ILP
+for model_name in $(ls -1 $MODEL_PATH/*.onnx); do
+    echo "* (TODO) Instantiate ILP $(basename ${model_name%.onnx})"
+    # MODEL_NAME=$(basename ${model_name%.onnx}) ./scripts/instantiate.sh
+done
+
--- scripts/create_linalg.sh
+++ scripts/create_linalg.sh
@@ -14,9 +14,6 @@ source $PYTHON_VENV_PATH
 MODEL_PATH="${MODEL_PATH:-$RAROG_ROOT/onnx_models}"
 MODEL_NAME="${MODEL_NAME:-model_1}"
 
-
-# echo "Running model_$MODEL_IDX"
-
 # mkdir -p tmp
 
 ONNX_MODEL="${MODEL_PATH}/${MODEL_NAME}.onnx"
@@ -24,10 +21,14 @@ MLIR_MODEL="${RAROG_ROOT}/tmp/${MODEL_NAME}.mlir"
 LINALG_MODEL="${RAROG_ROOT}/tmp/${MODEL_NAME}_linalg.mlir"
 
 # Convert ONNX model to MLIR (torch dialect)
-torch-mlir-import-onnx $ONNX_MODEL -o $MLIR_MODEL
+if ! [ -f $MLIR_MODEL ]; then
+    torch-mlir-import-onnx $ONNX_MODEL -o $MLIR_MODEL
+fi
 
 # Lower from torch to linalg dialect
-torch-mlir-opt \
-    --torch-onnx-to-torch-backend-pipeline \
-    --torch-backend-to-linalg-on-tensors-backend-pipeline \
-    $MLIR_MODEL -o $LINALG_MODEL
+if ! [ -f $LINALG_MODEL ]; then
+    torch-mlir-opt \
+        --torch-onnx-to-torch-backend-pipeline \
+        --torch-backend-to-linalg-on-tensors-backend-pipeline \
+        $MLIR_MODEL -o $LINALG_MODEL
+fi
