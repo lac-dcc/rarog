bf7ad6ab64ee0ea22d0168d18417ec88247a41ab
Elisa Fröhlich <elisaa.frohlich@gmail.com>
2026-06-05 18:22:46 -0300

    Adapt instrumentation scripts

--- scripts/lower_instrumented.sh
+++ scripts/lower_instrumented.sh
@@ -1,53 +1,15 @@
 #!/bin/bash
 
-usage() {
-    echo "Usage: $0 [-a] [-d] [-f] [-h]"
-    echo ""
-    echo "Options:"
-    echo "  -a              Enable allocation hoisting"
-    echo "  -d              Enable deallocation hoisting"
-    echo "  -f              Force recompile of rarog-opt"
-    echo "  -h              Show this help message"
-    exit 1
-}
-
 RAROG_ROOT="$(cd "$(dirname ${BASH_SOURCE[0]})/.." && pwd)"
 
-RAROG_OPT_PATH="${RAROG_ROOT}/build/bin/rarog-opt"
-
-ALLOCATION_HOISTING="${ALLOCATION_HOISTING:-''}"
-DEALLOCATION_HOISTING="${DEALLOCATION_HOISTING:-''}"
-FRESH=false
-
-while getopts "adfh" OPTION; do
-  case $OPTION in
-    a)  ALLOCATION_HOISTING="enable-reorder-mallocs" ;;
-    d)  DEALLOCATION_HOISTING="enable-reorder-frees" ;;
-    f)  FRESH=true ;;
-    h)  usage ;;
- esac
-done
-
-MODEL_NAME="${MODEL_NAME:-model_1}"
-
-if ! [ -f $RAROG_OPT_PATH ] || $FRESH
+source "${RAROG_ROOT}/scripts/config_args.sh" $@
+RETURN_CODE=$?
+if [[ $RETURN_CODE != 0 ]]
 then
-    # echo "rarog-opt is not compiled. Starting compilation process..."
-    cd $RAROG_ROOT
-    cmake -B build . --fresh
-    cmake --build build
-    if [[ $? != 0 ]]
-    then
-        echo "Compilation failed! Terminating..."
-        exit 1
-    fi
-    cd -
+    exit $RETURN_CODE
 fi
 
-LINALG_MODEL="${RAROG_ROOT}/tmp/${MODEL_NAME}_linalg.mlir"
-INSTRUMENTED_MODEL="${RAROG_ROOT}/tmp/${MODEL_NAME}_instrumented.mlir"
-
-if ! [ -f $LINALG_MODEL ]
+if ! [ -f $LINALG_MODEL ] || $FRESH
 then
     bash ${RAROG_ROOT}/scripts/create_linalg.sh
 fi
--- scripts/run_instrumented.sh
+++ scripts/run_instrumented.sh
@@ -1,65 +1,21 @@
 #!/bin/bash
 
-if [ -f ${RAROG_ROOT}/.env ]
-then
-    source ${RAROG_ROOT}/.env
-fi
-
-usage() {
-    echo "Usage: $0 [-a] [-d] [-f] [-h]"
-    echo ""
-    echo "Options:"
-    echo "  -a              Enable allocation hoisting"
-    echo "  -d              Enable deallocation hoisting"
-    echo "  -f              Force recompile of rarog-opt"
-    echo "  -h              Show this help message"
-    exit 1
-}
-
 RAROG_ROOT="$(cd "$(dirname ${BASH_SOURCE[0]})/.." && pwd)"
-ALLOCATION_HOISTING=""
-DEALLOCATION_HOISTING=""
-FRESH=""
 
-while getopts "adfh" OPTION; do
-  case $OPTION in
-    a)  ALLOCATION_HOISTING="-a" ;;
-    d)  DEALLOCATION_HOISTING="-d" ;;
-    f)  FRESH="-f" ;;
-    h)  usage ;;
- esac
-done
-
-MLIR_RUNNER=${MLIR_RUNNER:-mlir-runner}
-INSTRUMENTED_MALLOC="${RAROG_ROOT}/utils/libinstrumented_malloc.so"
-MLIR_UTILS=${MLIR_UTILS:-/usr/lib/llvm/lib/libmlir_runner_utils.so}
-MLIR_C_UTILS=${MLIR_C_UTILS:-/usr/lib/llvm/lib/libmlir_c_runner_utils.so}
-
-MODEL_NAME="${MODEL_NAME:-model_1}"
-
-if ! [ -f $RAROG_OPT_PATH ] || $FRESH
+source "${RAROG_ROOT}/scripts/config_args.sh" $@
+RETURN_CODE=$?
+if [[ $RETURN_CODE != 0 ]]
 then
-    cd $RAROG_ROOT
-    cmake -B build . --fresh
-    cmake --build build
-    if [[ $? != 0 ]]
-    then
-        echo "Compilation failed! Terminating..."
-        exit 1
-    fi
-    cd -
+    exit $RETURN_CODE
 fi
 
-INSTRUMENTED_MODEL="${RAROG_ROOT}/tmp/${MODEL_NAME}_instrumented.mlir"
-INSTRUMENTED_OUTPUT="${RAROG_ROOT}/tmp/${MODEL_NAME}.out"
-
-if ! [ -f $INSTRUMENTED_MODEL ]
+if ! [ -f $INSTRUMENTED_MODEL ] || $FRESH
 then
-    bash "${RAROG_ROOT}/scripts/lower_instrumented.sh" $ALLOCATION_HOISTING $DEALLOCATION_HOISTING &> /dev/null
+    bash "${RAROG_ROOT}/scripts/lower_instrumented.sh" $@
 fi
 
-time $MLIR_RUNNER \
-    $INSTRUMENTED_MODEL \
+/usr/bin/time --format="\ntime elapsed: %es\nmax memory used: %Mkb\nCPU used: %P" \
+    $MLIR_RUNNER $INSTRUMENTED_MODEL \
     --entry-point-result=void \
     --shared-libs=$INSTRUMENTED_MALLOC \
     --shared-libs=$MLIR_UTILS \
